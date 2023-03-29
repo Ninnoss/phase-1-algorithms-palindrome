b987b716597d5f431f106de91b6155cad413b1e1
Ninos Dinkha <ninos.dinkha97@gmail.com>
2023-03-29 23:31:31 +0300

    palindrome done

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const cleanedWord = word.replaceAll(' ', '');
+  const reverseWord = cleanedWord.split('').reverse().join('');
+  return cleanedWord === reverseWord;
 }
-
+isPalindrome('nurses run');
 /* 
   Add your pseudocode here
+  -reverse the word and add it to a variable
+  -compare original word with the reversed word
 */
 
 /*
@@ -13,13 +18,13 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('racecar'));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log('Expecting: false');
+  console.log('=>', isPalindrome('robot'));
 }
 
 module.exports = isPalindrome;
