1f2c7a9e3c3b1e920528add2f4947fd664ed5e62
Ninos Dinkha <ninos.dinkha97@gmail.com>
2023-03-31 00:03:37 +0300

    more solutions

--- index.js
+++ index.js
@@ -5,15 +5,44 @@ function isPalindrome(word) {
   return cleanedWord === reverseWord;
 }
 isPalindrome('nurses run');
-/* 
-  Add your pseudocode here
-  -reverse the word and add it to a variable
-  -compare original word with the reversed word
-*/
 
-/*
-  Add written explanation of your solution here
-*/
+
+// Solution 2
+function isPalindrome(word) {
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Solution 3
+function palindrome(str) {
+  let polishedString = str.replace(/\W+|_/g, '').toLowerCase();
+  let reversedString = polishedString.split('').reverse().join('');
+  return polishedString === reversedString ? true : false;
+}
+let result = palindrome('A man***, a p_lan, a canal. Panama');
+console.log(result);
+
+
+// Solution 4
+let palindrome = function (word) {
+  let len = word.length;
+  let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
+  let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
+  //console.log(start, end);
+  let flip = end.split('').reverse().join('');
+  return start === flip;
+};
+console.log(palindrome('radar')); //true
+console.log(palindrome('redder')); //true
+console.log(palindrome('window')); //false
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -25,6 +54,28 @@ if (require.main === module) {
 
   console.log('Expecting: false');
   console.log('=>', isPalindrome('robot'));
+
+  console.log('');
+
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('mom'));
+
+  console.log('');
+
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('abba'));
+
+  console.log('');
+
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('a'));
+
+  console.log('');
+
+  console.log('Expecting: false');
+  console.log('=>', isPalindrome('hi'));
+
+  console.log('');
 }
 
 module.exports = isPalindrome;
